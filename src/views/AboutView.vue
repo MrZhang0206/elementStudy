bff8c891464b2d5a29a14649ca0b5b94b08d22b2
MrZhang0206 <MrZhang0206@gmail.com>
2022-11-05 20:56:21 +0800

    lessAndEle

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,55 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <span class="num">213</span>
+    <el-input v-model="input" placeholder="Please input" />
+    <el-button type="success" @click="getInput()">getInput</el-button>
+  </div>
+  <div class="about2">
+    <span class="num">123</span>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<script>
+export default {
+  data() {
+    return {
+      input: "",
+    };
+  },
+  methods: {
+    getInput: function () {
+      console.log(this.input);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import "@/assets/index.less";
+.num {
+  color: @color-blue;
+}
+.about {
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  .num {
+    color: @color-red;
+  }
+}
+.about2 {
+  width: 100px;
+  height: 50px;
+  border: @border;
+  .test();
+}
+/deep/ .el-input {
+  .el-input__wrapper {
+    background: @text-red;
+    .el-input__inner {
+      background: @text-red;
+    }
   }
 }
 </style>
